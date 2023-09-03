a2429adb787707061e5b520469212e197b9f05b4
MedicBehindYou <byy0url3ave@gmail.com>
2023-09-02 23:44:18 -0500

    Added a DB backup script and wrapped kyanite_db (the mainscript) in a try...finally block

---
+++ db_backup.py
@@ -0,0 +1,50 @@
+# db_backup.py
+
+import os
+import shutil
+import time
+from logger import log  # Import the log function from the logger module
+
+# Directory where backup files will be stored
+BACKUP_DIR = 'backup/'
+
+# Ensure the backup directory exists
+if not os.path.exists(BACKUP_DIR):
+    os.makedirs(BACKUP_DIR)
+
+# Function to create a backup of the database
+def create_backup():
+    try:
+        # Generate a unique backup file name using a timestamp
+        backup_file = f'{BACKUP_DIR}backup_{int(time.time())}.db'
+        
+        # Copy the current database file to the backup location
+        shutil.copy('database.db', backup_file)
+        
+        log(f'Database backed up to {backup_file}')
+    except Exception as e:
+        log(f'Error creating database backup: {e}')
+
+# Function to manage backups and retain up to five backups
+def manage_backups():
+    try:
+        # Get a list of backup files in the backup directory
+        backups = [os.path.join(BACKUP_DIR, file) for file in os.listdir(BACKUP_DIR) if file.startswith('backup_')]
+        
+        # Sort the list by modification time (oldest first)
+        backups.sort(key=lambda x: os.path.getmtime(x))
+        
+        # Check the number of backups
+        if len(backups) >= 5:
+            # Delete the oldest backup if there are more than five
+            os.remove(backups[0])
+            log(f'Oldest backup deleted: {backups[0]}')
+        
+        # Create a new backup
+        create_backup()
+    except Exception as e:
+        log(f'Error managing backups: {e}')
+
+if __name__ == "__main__":
+    create_backup()  # Create a backup at the start of the script's run
+    manage_backups()  # Manage backups to retain up to five
--- kyanite_db.py
+++ kyanite_db.py
@@ -10,6 +10,7 @@ import sys
 from logger import log
 from importer import bulk_import_tags, single_import  # Import the bulk and single import functions
 from db_setup import setup_database
+from db_backup import create_backup, manage_backups
 
 # Check if the "--setup" switch is provided
 if len(sys.argv) > 1 and sys.argv[1] == "--setup":
@@ -32,81 +33,89 @@ if len(sys.argv) > 1 and sys.argv[1] == "--single":
         print("Usage: --single <tag_name>")
     sys.exit()
 
-# This creates a connection to a new or existing database file
-connection = sqlite3.connect('database.db')
+try:
+    # Create a backup at the start of the script's run
+    create_backup()
 
-# A cursor is used to execute SQL commands and fetch results.
-cursor = connection.cursor()
+    # This creates a connection to a new or existing database file
+    connection = sqlite3.connect('database.db')
 
-# Define the maximum inactivity time (in seconds) before considering the subprocess inactive
-MAX_INACTIVITY_TIME = 1200  # Adjust this value as needed
+    # A cursor is used to execute SQL commands and fetch results.
+    cursor = connection.cursor()
 
-# Initialize log file
-log_file = open('log.txt', 'a')
+    # Define the maximum inactivity time (in seconds) before considering the subprocess inactive
+    MAX_INACTIVITY_TIME = 1200  # Adjust this value as needed
 
-def inactivity_checker(process, tag):
-    while process.poll() is None:
-        current_time = time.time()
-        if current_time - subprocess_start_time > MAX_INACTIVITY_TIME:
-            log(f'Subprocess for tag "{tag}" closed due to inactivity.')
-            process.kill()
+    # Initialize log file
+    log_file = open('log.txt', 'a')
+
+    def inactivity_checker(process, tag):
+        while process.poll() is None:
+            current_time = time.time()
+            if current_time - subprocess_start_time > MAX_INACTIVITY_TIME:
+                log(f'Subprocess for tag "{tag}" closed due to inactivity.')
+                process.kill()
+                break
+            time.sleep(1)
+
+    while True:
+        # Get the first tag with a status of "0"
+        cursor.execute('SELECT name FROM tags WHERE complete = 0 LIMIT 1')
+        row = cursor.fetchone()
+
+        if row:
+            tag = row[0]  # Assuming the "name" column is the first column in the query result
+            log(f'Starting processing tag: {tag}')
+        else:
+            update_query = "UPDATE tags SET complete = 0;"
+            cursor.execute(update_query)
+            connection.commit()
+            log('All tags processed. Resetting for a new run.')
             break
-        time.sleep(1)
 
-while True:
-    # Get the first tag with a status of "0"
-    cursor.execute('SELECT name FROM tags WHERE complete = 0 LIMIT 1')
-    row = cursor.fetchone()
+        # Bash command to execute
+        command = ["/app/kyanite", "-t", tag]
 
-    if row:
-        tag = row[0]  # Assuming the "name" column is the first column in the query result
-        log(f'Starting processing tag: {tag}')
-    else:
-        update_query = "UPDATE tags SET complete = 0;"
-        cursor.execute(update_query)
-        connection.commit()
-        log('All tags processed. Resetting for a new run.')
-        break
+        log(f'Starting subprocess for tag: {tag}')
 
-    # Bash command to execute
-    command = ["/app/kyanite", "-t", tag]
-
-    log(f'Starting subprocess for tag: {tag}')
-
-    # Execute the Bash command and capture the output
-    process = subprocess.Popen(command, shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
-    subprocess_start_time = time.time()
-
-    # Start the inactivity checker thread
-    inactivity_thread = threading.Thread(target=inactivity_checker, args=(process, tag))
-    inactivity_thread.start()
-
-    for line in process.stdout:
-        print(line, end='')
+        # Execute the Bash command and capture the output
+        process = subprocess.Popen(command, shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
         subprocess_start_time = time.time()
 
-    # Wait for the subprocess to finish and join the inactivity checker thread
-    process.wait()
-    inactivity_thread.join()
+        # Start the inactivity checker thread
+        inactivity_thread = threading.Thread(target=inactivity_checker, args=(process, tag))
+        inactivity_thread.start()
 
-    log(f'Subprocess for tag "{tag}" finished with return code: {process.returncode}')
+        for line in process.stdout:
+            print(line, end='')
+            subprocess_start_time = time.time()
 
-    # Only update database if subprocess completed without being killed by inactivity checker
-    if process.returncode == 0:
-        try:
-            current_timestamp = datetime.now()  # Get the current timestamp
-            cursor.execute("BEGIN")
-            cursor.execute("UPDATE tags SET complete = 1, date = ? WHERE name = ?", (current_timestamp, tag))
-            cursor.execute("COMMIT")
-            log(f'Tag "{tag}" processed successfully.')
-        except Exception as e:
-            cursor.execute("ROLLBACK")
-            error_message = f'Error processing tag "{tag}": {e}'
-            log(error_message)
+        # Wait for the subprocess to finish and join the inactivity checker thread
+        process.wait()
+        inactivity_thread.join()
 
-        connection.commit()
-    elif process.returncode is not None:
-        log(f'Subprocess for tag "{tag}" was terminated with return code: {process.returncode}')    
+        log(f'Subprocess for tag "{tag}" finished with return code: {process.returncode}')
 
-connection.close()
-log_file.close()
+        # Only update database if subprocess completed without being killed by inactivity checker
+        if process.returncode == 0:
+            try:
+                current_timestamp = datetime.now()  # Get the current timestamp
+                cursor.execute("BEGIN")
+                cursor.execute("UPDATE tags SET complete = 1, date = ? WHERE name = ?", (current_timestamp, tag))
+                cursor.execute("COMMIT")
+                log(f'Tag "{tag}" processed successfully.')
+            except Exception as e:
+                cursor.execute("ROLLBACK")
+                error_message = f'Error processing tag "{tag}": {e}'
+                log(error_message)
+
+            connection.commit()
+        elif process.returncode is not None:
+            log(f'Subprocess for tag "{tag}" was terminated with return code: {process.returncode}')    
+
+finally:
+    # Manage Backups before exiting
+    manage_backups()
+
+    connection.close()
+    log_file.close()
